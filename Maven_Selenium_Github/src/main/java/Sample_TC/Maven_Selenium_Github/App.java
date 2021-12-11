cc8a6da8fe8784f9a88867c54ed097e5a58d0bb3
DCS <DCS@DESKTOP-MH4TSPO>
2021-12-10 21:16:19 -0800

    Update Project

---
+++ Maven_Selenium_Github/src/main/java/Sample_TC/Maven_Selenium_Github/App.java
@@ -0,0 +1,13 @@
+package Sample_TC.Maven_Selenium_Github;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
